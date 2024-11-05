3dae86a2e81a25d1a03f37a7790ccda2fdc3f459
Zong-Zehao <150991855+Zong-Zehao@users.noreply.github.com>
2024-11-05 21:35:23 +0800

    added model and controller for showing new thread

--- command.sql
+++ command.sql
@@ -13,3 +13,13 @@ INSERT INTO Users (username, password, role, reputation) VALUES
 ('john_doe', 'password123', 'admin', 100),
 ('jane_smith', 'password456', 'user', 50),
 ('adminnew', '123', 'admin', 100)
+
+
+CREATE TABLE Threads ( 
+    thread_id INT PRIMARY KEY IDENTITY(1,1),  -- Unique identifier for each thread 
+    title VARCHAR(255) NOT NULL,               -- Title of the thread 
+    content TEXT NOT NULL,                      -- Content of the thread 
+    date DATE DEFAULT CURRENT_TIMESTAMP,      -- Date when the thread is created 
+    username VARCHAR(255) NOT NULL,            -- Username of the user who created the thread 
+    FOREIGN KEY (username) REFERENCES Users(username)  -- Reference to the Users table 
+);
