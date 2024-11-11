15f22ebafaa45f4c8428b0f5d508c0228f199996
izwnsyhrizt <izwnsyhrizt@gmail.com>
2024-11-11 11:53:35 +0800

    added categories to create thread

--- command.sql
+++ command.sql
@@ -16,12 +16,13 @@ VALUES ('johndoe', 'password123', 'admin', 100);
 
 CREATE TABLE Threads ( 
     thread_id INT PRIMARY KEY IDENTITY(1,1),  -- Unique identifier for each thread 
-    title VARCHAR(255) NOT NULL,               -- Title of the thread 
+    title VARCHAR(255) NOT NULL,               -- Title of the thread
+    category VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,                      -- Content of the thread 
     date DATE DEFAULT CURRENT_TIMESTAMP,      -- Date when the thread is created 
     username VARCHAR(255) NOT NULL,            -- Username of the user who created the thread 
     FOREIGN KEY (username) REFERENCES Users(username)  -- Reference to the Users table 
-);
+)
 
 -- Create Replies table
 CREATE TABLE Replies (
